74388c17543d2029bcb86243d5f76c0369febfda
Hans Sigrist <hsigrist@gmail.com>
2018-05-05 21:48:25 -0300

    added inecuaciones-lineales

---
+++ 3TP/TALKS/auto/ECUACION-SEGUNDO-GRADO-3TP-41.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "ECUACION-SEGUNDO-GRADO-3TP-41"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "pdfpages"))
+ :latex)
+
---
+++ 3TP/TALKS/auto/METODOS-RESOLUCION-41.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "METODOS-RESOLUCION-41"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "pdfpages"))
+ :latex)
+
